60408f5358334e395f892ee36b0f99424321783e
jeremy <jeremyeparker@yahoo.com>
2023-01-04 17:54:58 -0500

    renaming dir instead of file names

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
+//Node.js file system module allows you to work with the file system
 const fs = require('fs');
+//Extract the filename from a file path:
 const path = require('path');
-
+//Location of folder or path
 const filePath = '/home/jar/Desktop/New Folder/';
-const files = fs.readdirSync(filePath).filter((file) => file.endsWith('.png'));
+//used to synchronously read the contents of a given directory. The method returns an array with all the file names or objects in the directory.
+const files = fs.readdirSync(filePath).filter((file) => file.endsWith('.jpg'));
 
 let count = 1;
-const extName = '.png';
+// const extName = '.png';
+/*
 files.forEach(file => {
 
     const oldPath = path.join(filePath, file)
-    const newPath = path.join(filePath, count + extName)
+    const newPath = path.join(filePath, count + '.jpg')
     fs.renameSync(oldPath, newPath)
     console.log('Done', count, '...');
     count++;
 })
+*/
